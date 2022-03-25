59d693d71538eb4c34e06ba5815e91d20ab3c869
Kasir Barati <kasir.barati@gmail.com>
2022-03-25 18:07:30 +0430

    update: try other ways

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -28,18 +28,45 @@ async function main() {
 
     console.log(rawQuery);
 
-    const allCities = await prisma.$executeRaw`
-                SELECT  *, POSITION(
-                    LOWER('${filters.query}') 
-                    IN 
-                    LOWER(${lowerFieldName})
-                ) AS indexOfTheSearchedName FROM cities
-                WHERE ${lowerFieldName} 
-                    ILIKE '%${filters.query}%'
-                ORDER BY 
-                    indexOfTheSearchedName ASC, 
-                    ${orderByFieldName} ASC;
-            `;
+    // No one of this syntax works:
+    // #1
+    // const allCities = await prisma.$queryRaw`
+    // #2
+    // const allCities = await prisma.$executeRaw`
+    //             SELECT  *, POSITION(
+    //                 LOWER('${filters.query}')
+    //                 IN
+    //                 LOWER(${lowerFieldName})
+    //             ) AS indexOfTheSearchedName FROM cities
+    //             WHERE ${lowerFieldName}
+    //                 ILIKE '%${filters.query}%'
+    //             ORDER BY
+    //                 indexOfTheSearchedName ASC,
+    //                 ${orderByFieldName} ASC;
+    //         `;
+    // #3
+    // const allCities =
+    //     await prisma.$queryRaw(Prisma.sql`SELECT  *, POSITION(
+    //         LOWER('${filters.query}')
+    //         IN
+    //         LOWER(${lowerFieldName})
+    //     ) AS indexOfTheSearchedName FROM cities
+    //     WHERE ${lowerFieldName}
+    //         ILIKE '%${filters.query}%'
+    //     ORDER BY
+    //         indexOfTheSearchedName ASC,
+    //         ${orderByFieldName} ASC;`);
+
+    // This one throw different error
+    // #3
+    const allCities = await prisma.$queryRawUnsafe(
+        "SELECT  *, POSITION(LOWER('?') IN LOWER(?)) AS indexOfTheSearchedName FROM cities WHERE ? ILIKE '%?%' ORDER BY indexOfTheSearchedName ASC, ? ASC;",
+        filters.query,
+        lowerFieldName,
+        lowerFieldName,
+        filters.query,
+        orderByFieldName,
+    );
 
     console.dir(allCities, { depth: null });
 }
